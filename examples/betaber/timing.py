3bbe3e1e9b1088702037b36515e4c098316a974f
McCoy Becker <mccoybecker@gmail.com>
2025-06-12 12:23:53 -0400

    Add beta-bernoulli benchmark.

--- examples/betaber/timing.py
+++ examples/betaber/timing.py
@@ -61,6 +61,74 @@ def genjax_timing(
     return times, (time_mu, time_std)
 
 
+def numpyro_timing(
+    num_obs=50,
+    repeats=200,
+    num_samples=1000,
+):
+    import numpyro
+    import numpyro.distributions as dist
+    from numpyro.handlers import block, replay, seed, substitute, trace
+    from numpyro.infer.util import (
+        log_density,
+    )
+
+    key = jax.random.PRNGKey(314159)
+    data = jnp.ones(num_obs)
+
+    def model(data):
+        # define the hyperparameters that control the Beta prior
+        alpha0 = 10.0
+        beta0 = 10.0
+        # sample f from the Beta prior
+        f = numpyro.sample("latent_fairness", dist.Beta(alpha0, beta0))
+        # loop over the observed data
+        with numpyro.plate("data", size=len(data)):
+            # observe datapoint i using the Bernoulli
+            # likelihood Bernoulli(f)
+            numpyro.sample("obs", dist.Bernoulli(f), obs=data)
+
+    guide = block(model, hide=["obs"])
+
+    def importance(model, guide):
+        def fn(key, *args, **kwargs):
+            key, sub_key = jax.random.split(key)
+            seeded_guide = seed(guide, sub_key)
+            guide_log_density, guide_trace = log_density(
+                seeded_guide,
+                args,
+                kwargs,
+                {},
+            )
+            seeded_model = seed(model, key)
+            replay_model = replay(seeded_model, guide_trace)
+            model_log_density, model_trace = log_density(
+                replay_model,
+                args,
+                kwargs,
+                {},
+            )
+            return model_log_density - guide_log_density
+
+        return fn
+
+    vectorized_importance_weights = jax.jit(
+        jax.vmap(importance(model, guide), in_axes=(0, None)),
+    )
+
+    sub_keys = jax.random.split(key, num_samples)
+
+    # Run to warm up the JIT.
+    _ = vectorized_importance_weights(sub_keys, data)
+    _ = vectorized_importance_weights(sub_keys, data)
+
+    times, (time_mu, time_std) = timing(
+        lambda: vectorized_importance_weights(sub_keys, data).block_until_ready(),
+        repeats=repeats,
+    )
+    return times, (time_mu, time_std)
+
+
 def handcoded_timing(
     num_obs=50,
     repeats=50,
@@ -95,7 +163,103 @@ def handcoded_timing(
     return times, (time_mu, time_std)
 
 
+def timing_comparison_fig(
+    num_obs=50,
+    repeats=200,
+    num_samples=1000,
+):
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+
+    sns.set_style("white")
+
+    gj_times, (gj_mu, gj_std) = genjax_timing(
+        repeats=repeats,
+        num_obs=num_obs,
+        num_samples=num_samples,
+    )
+    np_times, (np_mu, np_std) = numpyro_timing(
+        repeats=repeats,
+        num_obs=num_obs,
+        num_samples=num_samples,
+    )
+    hc_times, (hc_mu, hc_std) = handcoded_timing(
+        repeats=repeats,
+        num_obs=num_obs,
+        num_samples=num_samples,
+    )
+    print(gj_mu, hc_mu, np_mu)
+
+    fig, ax = plt.subplots(figsize=(5, 3), dpi=240)
+    ax.ticklabel_format(style="sci", scilimits=(-3, 4), axis="both")
+    ax.set_xlabel("Time (s)")
+    ax.set_ylabel("Counts")
+
+    # GenJAX
+    ax.hist(
+        gj_times,
+        bins=20,
+        color="deepskyblue",
+        label="GenJAX",
+    )
+    ax.axvline(
+        x=gj_mu,
+        color="black",
+        linestyle="-",
+        linewidth=2,
+    )  # Outline
+    ax.axvline(
+        x=gj_mu,
+        color="deepskyblue",
+        linestyle="--",
+        linewidth=1,
+    )
+
+    # NumPyro
+    ax.hist(
+        np_times,
+        bins=20,
+        color="coral",
+        label="NumPyro",
+    )
+    ax.axvline(
+        x=np_mu,
+        color="black",
+        linestyle="-",
+        linewidth=2,
+    )  # Outline
+    ax.axvline(
+        x=np_mu,
+        color="coral",
+        linestyle="--",
+        linewidth=1,
+    )
+
+    # Handcoded
+    ax.hist(
+        hc_times,
+        bins=20,
+        color="gold",
+        label="Handcoded",
+    )
+    ax.axvline(
+        x=hc_mu,
+        color="black",
+        linestyle="-",
+        linewidth=2,
+    )  # Outline
+    ax.axvline(
+        x=hc_mu,
+        color="gold",
+        linestyle="--",
+        linewidth=1,
+    )
+
+    handles, labels = ax.get_legend_handles_labels()
+    ax.legend(handles, labels)
+    plt.tight_layout()
+    plt.savefig("examples/betaber/figs/comparison.pdf")
+
+
 if __name__ == "__main__":
-    _, (mu, _) = genjax_timing(num_samples=1000)
-    _, (mu_, _) = handcoded_timing(num_samples=1000)
-    print(mu, mu_)
+    timing_comparison_fig()
